7e6acb60ba05396d3128bdd4ae81c18e098ad1bb
masterhash-us <32805608+masterhash-us@users.noreply.github.com>
2018-01-31 13:08:54 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -138,7 +138,7 @@ rm Escrow.tgz
 # Install Escrow
 echo && echo "Installing Escrow..."
 sleep 3
-sudo mv /root/escrow{d,-cli} /usr/local/bin
+cp Escrowd /usr/local/bin
 
 # Create config for Escrow
 echo && echo "Configuring Escrow..."
